4b52ec729c9ef985a49e582fecd0a7d80b4d150d
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-07-11 21:06:42 +0800

    feat: Remove wrapping promise with PromiseLike.

--- src/types/promise.ts
+++ src/types/promise.ts
@@ -1,2 +1,4 @@
-export type Promisify<T> = T extends Promise<unknown> ? T : Promise<T>;
-export type UnPromisefy<T> = T extends Promise<infer R> ? UnPromisefy<R> : T;
+export type Promisify<T> = T extends PromiseLike<unknown> ? T : Promise<T>;
+export type UnPromisefy<T> = T extends PromiseLike<infer R>
+  ? UnPromisefy<R>
+  : T;
