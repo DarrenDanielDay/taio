a6d748c2ebe6f2d027aad3525ecc3ac73126dddb
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-07-31 19:56:26 +0800

    chore: Build script in three formats and jest config.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
 import type { InitialOptionsTsJest } from "ts-jest/dist/types";
 import { defaults as tsjPreset } from "ts-jest/presets";
 const config: InitialOptionsTsJest = {
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
   transform: {
     ...tsjPreset.transform,
   },
--- scripts/esbuild-bundle.ts
+++ scripts/esbuild-bundle.ts
@@ -1,8 +1,26 @@
 import * as esbuild from "esbuild";
 import * as glob from "glob";
-esbuild.buildSync({
+const commonConfig: esbuild.BuildOptions = {
   tsconfig: "./tsconfig.json",
   entryPoints: [...glob.sync("src/**/*.ts")],
   sourcemap: true,
   outdir: "build",
+};
+esbuild.buildSync({
+  ...commonConfig,
+  format: "esm",
+  outExtension: {
+    ".js": ".mjs",
+  },
+});
+esbuild.buildSync({
+  ...commonConfig,
+  format: "cjs",
+  outExtension: {
+    ".js": ".cjs",
+  },
+});
+esbuild.buildSync({
+  ...commonConfig,
+  format: "cjs",
 });
